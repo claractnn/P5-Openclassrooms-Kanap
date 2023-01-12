c34b1a70cedb3e97472617657a238558cba7a422
Clara Coatanoan <ccoatanoan@gmail.com>
2023-01-12 18:17:01 +0100

    réécriture

--- front/js/product.js
+++ front/js/product.js
@@ -48,65 +48,120 @@ fetch(apiProductUrl)
 let selectColor = document.getElementById("colors");
 let selectQuantity = document.getElementById('quantity');
 
-// Définir les éléments du tableau 
-let color = selectColor.value;
-let quantity = selectQuantity.value;
-
 // Modifier le tableau en ajoutant des eventlistener quand l'utilisateur choisi une couleur ou un nombre d'articles
 selectColor.addEventListener("input", e => color = e.target.value);
 selectQuantity.addEventListener("input", e => quantity = e.target.value);
 
 // Créer une variable pour le bouton "ajouter au panier"
 const button = document.querySelector('#addToCart');
-
-// Créer un eventListener pour le bouton "ajouter au panier" avec certaines conditions 
+// Créer un eventListener pour le bouton "ajouter au panier"  
 button.addEventListener("click", (event) => {
-    // Créer un objet avec tous les éléments du produit 
-    let productDetails = {
+    // Définir la couleur et la quantité choisies à leurs valeurs respectives
+    let color = selectColor.value;
+    let quantity = selectQuantity.value;
+
+    // Créer un objet du tableau pour le panier
+    let cart = [{
         id: id,
         color: color,
-        quantity: Number(quantity),
-        price: Number(priceElement.textContent)
-    }
+        quantity: Number(quantity)
+    }];
+
+    // Ajouter des conditions si les options du produit n'ont pas été choisies
     if (color == '') {
         alert('Veuillez sélectionner une couleur')
     } else if (quantity < 1 || quantity == NaN) {
         alert('Veuillez indiquer un nombre d\'articles')
     } else if (quantity > 100) {
         alert('Veuillez indiquer un nombre d\'articles inférieur à 100')
-    } else {
-        addProductConfirm(productDetails)
-    }
-    //ajouter la future fonction qui affiche la page panier ?
+    } 
 
     // Convertir les clés et valeurs de l'objet en string (car localStorage ne peut pas storer d'objet, renvoie : "[object Object]") 
-    window.localStorage.setItem("productDetails", JSON.stringify(productDetails));
+    window.localStorage.setItem("panier", JSON.stringify(cart));
 
-    // Créer une variable pour récupérer l'objet (parse the string) 
-    let addProductDetails = window.localStorage.getItem("productDetails");
-    console.log(JSON.parse(addProductDetails));
+    // Créer une variable pour récupérer l'objet panier (parse the string) 
+    let getCart = window.localStorage.getItem("panier");
+    console.log(JSON.parse(getCart));
+
+    // Vérifier si le panier est vide si toutes les conditions sont remplies
+    let basket = getCart(); 
+    if (basket.lenght == 0) {
+        // Créer une fonction qui récupère les éléments du panier 
+        function getBasket() {
+            // Créer une variable qui définie la récupération des éléments du panier
+            let productInLocalStorage = localStorage.getItem("panier")
+            if (productInLocalStorage == null) {
+                return []
+            } else {
+                
+            }
+        }
+        // Créer une fonction pour ajouter les éléments du produit choisis dans le panier
+        function addBasket(cart) {
+            let productInLocalStorage = getBasket();
+        }
+    }
+
+    if (basket == null || basket.lenght == 0) {
+        // Si le panier n'existe pas, le créer en objet dans un tableau 
+        cart = [{
+        id: id,
+        color: color,
+        quantity: Number(quantity),
+        //price: Number(priceElement.textContent)
+        }];
+
+        // Créer une fonction d'ajout au panier 
+        function addBasket(cart) {
+        // Récupérer les éléments du produit choisi par l'utilisateur
+        let productInLocalStorage = getBasket();
+        };
+
+        // Créer un message d'alerte pour confirmer l'ajout du produit au panier
+        let addBasketConfirm = () => {
+            alert('Le produit a bien été ajouté au panier')
+        addBasketConfirm(cart)   // ???? PLACEMENT AVANT OU APRÈS L'ACCOLADE ????
+        }
+    } else {
+        // Vérifier si 2 produits sont similaires 
+        const found = false;
+
+            
+        
+    }
+
+    
 });
 
 // Créer un message pour confirmer que le produit a bien été ajouté au panier
-let addProductConfirm = () => {
-    alert('Le produit a bien été ajouté au panier')
+/*
+// else {
+    addProductConfirm(productDetails)
 }
 
-// Définir le contenu du panier 
-// let basket =  
-
+}
+*/
 
 /* Créer une fonction pour l'ajout du/des produit(s) sélectionné(s) par l'utilisateur dans le panier
 function addToCart(productDetails) {}  */
-
-
-
-
 // Redirection sur une url relative cart.html une fois qu'on a ajouté au panier ?
 //window.location.href = "cart.html"
-        
 
+// Créer une fonction pour enregistrer le panier
+function saveBasket(productInLocalStorage) {
+    localStorage.setItem("panier", JSON.stringify(productInLocalStorage))
+}
 
-
-
+// Créer une fonction de récupération du panier
+function getBasket() {
+    // Récupérer les informations du panier existant (choisies par l'utilisateur)
+    let productInLocalStorage = localStorage.getItem("panier");
+    if (productInLocalStorage == null) {
+        // Retourner un tableau vide si le panier est vide
+        return [];
+    } else {
+        // Sinon retourner le contenu du localStorage
+        return JSON.parse(productInLocalStorage)
+    }
+}
 
