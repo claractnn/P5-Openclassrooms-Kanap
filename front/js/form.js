f7e147b2ddf86dd444c5d1ef671c8fc823cf2d26
Clara Coatanoan <ccoatanoan@gmail.com>
2023-02-07 11:54:47 +0100

    ajout event listener

--- front/js/form.js
+++ front/js/form.js
@@ -6,6 +6,7 @@ const lastName = document.querySelector('#lastName');
 const address = document.querySelector('#address');
 const city = document.querySelector('#city');
 const email = document.querySelector('#email');
+console.log(email.value)
 
 // Déclarer les variables pour les erreurs de chaque input du formulaire
 const firstNameError = firstName.nextElementSibling;
@@ -17,8 +18,38 @@ const emailError = email.nextElementSibling;
 // Créer les RegExp 
 const nameCityRegExp = new RegExp(/^[a-zA-ZÀ-ÿ]+(['-]?[a-zA-ZÀ-ÿ]+)$/);
 const addressRegExp = new RegExp(/^[0-9]{1,3}[a-zA-ZÀ-ÿ -,']+$/);
-const emailRegExp = new RegExp(/^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9]+[.]{1}[a-z]{2,10}$/)
+const emailRegExp = new RegExp(/^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9]+[.]{1}[a-z]{2,10}$/);
 
+// Créer une fonction de contrôle du formulaire 
+function controlForm() {
+    // Créer une fonction qui test l'expréssion régulière
+    function testRegExp(name, regExp, error) {
+        if(name.value.match(regExp)) {
+            error.innerHTML = "";
+        } else {
+            error.innerHTML = "La saisie est incorrecte";
+            return false
+        }
+    }
 
+    // Écouter les événements correspondant à chaque input
+    firstName.addEventListener('change', function() {
+        testRegExp(firstName, nameCityRegExp, firstNameError);
+    });
+    lastName.addEventListener('change', function() {
+        testRegExp(lastName, nameCityRegExp, lastNameError);
+    });
+    address.addEventListener('change', function() {
+        testRegExp(address, addressRegExp, addressError);
+    });
+    city.addEventListener('change', function() {
+        testRegExp(city, nameCityRegExp, cityError);
+    });
+    email.addEventListener('change', function() {
+        testRegExp(email, emailRegExp, emailError);
+    });
+};
+
+controlForm();
 // Créer les différentes fonctions pour la validation des formats 
 
