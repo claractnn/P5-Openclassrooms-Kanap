34370d4d44f1463328f4fe82ba92446de0483177
Clara Coatanoan <ccoatanoan@gmail.com>
2023-02-14 10:53:43 +0100

    ajout commentaire

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,4 +1,4 @@
-
+//Récupérer l'id de la commande pour afficher le numéro de commande
 const id = new URL(window.location.href).searchParams.get("orderid");
 console.log(id)
 const orderId = document.getElementById('orderId');
